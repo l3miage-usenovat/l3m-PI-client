cc01e02f7e6adb0c2981e2092b5e4f2cf287c90f
l3miage-usenovat <tin.usenova@gmail.com>
2021-04-15 09:44:28 +0200

    Les methodes login et logout dans app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
+import firebase from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class AppComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
+
+  /*MON CODE SUIVI DE ONENOTE*/
+
+  constructor(public auth: AngularFireAuth) {}
+
+    login(): void {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      provider.setCustomParameters({
+        prompt: 'select_account'
+      });
+      this.auth.signInWithPopup(provider);
+    }
+
+    logout(): void {
+      this.auth.signOut();
+    }
 }

6293ad2a96b91b9421a90a47eebc228e1d0a4bdf
l3miage-usenovat <tin.usenova@gmail.com>
2021-04-16 12:25:00 +0200

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyDY01V9z3TPIMe110LRUvzX2oW41WXyjTc",
+    authDomain: "chamis-53c3b.firebaseapp.com",
+    projectId: "chamis-53c3b",
+    storageBucket: "chamis-53c3b.appspot.com",
+    messagingSenderId: "500121318408",
+    appId: "1:500121318408:web:fceb4263e59788cf668dbd",
+    measurementId: "G-Q76QN94LE6"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,20 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import firebase from "firebase";
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyDY01V9z3TPIMe110LRUvzX2oW41WXyjTc",
+    authDomain: "chamis-53c3b.firebaseapp.com",
+    projectId: "chamis-53c3b",
+    storageBucket: "chamis-53c3b.appspot.com",
+    messagingSenderId: "500121318408",
+    appId: "1:500121318408:web:fceb4263e59788cf668dbd",
+    measurementId: "G-Q76QN94LE6"
+  },
 };
 
 /*
@@ -14,3 +25,7 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+////////////////////////////////////////////////////Ce que j'ai copié sur firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
